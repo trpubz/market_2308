7896ed0a4e8d7d5b322df98838bd8e11eb2f46d3
Taylor Pubins <tpubz@icloud.com>
2023-09-18 10:21:19 -0700

    Add #total_inventory #overstocked_items #sorted_item_list to ::Market

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,36 @@ class Market
   def vendors_that_sell(item)
     @vendors.filter { |vendor| vendor.inventory.include?(item) }
   end
+
+  def total_inventory
+    uniq_items = @vendors.flat_map { |vendor| vendor.inventory.keys }.uniq
+
+    inventory = Hash.new({})
+    uniq_items.each { |item| inventory[item] = {} }
+    inventory.each do |item, qty_hash|
+      qty_hash[:quantity] = @vendors.reduce(0) do |tot, vendor|
+        tot + vendor.check_stock(item)
+      end
+      qty_hash[:vendors] = @vendors.filter { |vendor| vendor.inventory[item] > 0 }
+      # require "byebug"; byebug
+      inventory[item] = qty_hash
+    end
+
+    inventory
+  end
+
+  def overstocked_items
+    overstocked = []
+    total_inventory.each do |item, qty_hash|
+      if qty_hash[:quantity] > 50 && qty_hash[:vendors].size > 1
+        overstocked << item
+      end
+    end
+
+    overstocked
+  end
+
+  def sorted_item_list
+    total_inventory.keys.map(&:name).sort
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe Market do
     @vendor1.stock(@item1, 35)
     @vendor1.stock(@item2, 7)
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
-    @vendor2.stock(@item4, 50)
     @vendor2.stock(@item3, 25)
+    @vendor2.stock(@item4, 50)
     @vendor3 = Vendor.new("Palisade Peach Shack")
     @vendor3.stock(@item1, 65)
   end
@@ -58,6 +58,66 @@ RSpec.describe Market do
     it "returns vendor objects that sell item" do
       expect(@market.vendors_that_sell(@item1)).to eq [@vendor1, @vendor3]
       expect(@market.vendors_that_sell(@item4)).to eq [@vendor2]
+    end
+  end
+
+  describe "#total_inventory" do
+    before do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+    end
+
+    it "returns {items => {qty => [vendors]}}" do
+      expect(@market.total_inventory).to be_a Hash
+      # expect(@market.total_inventory).to eq({@item1 =>
+      #                                          {
+      #                                            quantity: 100,
+      #                                            vendors: [@vendor1, @vendor3]
+      #                                          },
+      #                                       @item2 =>
+      #                                          {
+      #                                            quantity: 7,
+      #                                            vendors: [@vendor1]
+      #                                          },
+      #                                       @item3 =>
+      #                                          {
+      #                                            quantity: 25,
+      #                                            vendors: [@vendor3]
+      #                                          },
+      #                                       @item4 =>
+      #                                          {
+      #                                            quantity: 50,
+      #                                            vendors: [@vendor3]
+      #                                          }})
+    end
+  end
+
+  describe "#overstocked_items" do
+    before do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+    end
+
+    it "returns Array of [Items] that are overstocked" do
+      # item is overstocked if it is sold by more than 1 vendor AND the total quantity is greater than 50.
+      expect(@market.overstocked_items).to eq [@item1]
+    end
+  end
+
+  describe "#sorted_item_list" do
+    before do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+    end
+
+    it "returns [Items.names] alphabetically" do
+      expect(@market.sorted_item_list).to eq ["Banana Nice Cream",
+        "Peach",
+        "Peach-Raspberry Nice Cream",
+        "Tomato"]
     end
   end
 end
